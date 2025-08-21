9b3700db015427e9a4ed2dbfb4182bcf29a89014
Ritupagar12 <ritupagar3@gmail.com>
2025-08-21 20:39:13 +1200

    Added simple interest code

---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter Principal: "))
+rateOfInterest= float(input("Enter Rate of Interest: "))
+time = float(input("Enter Time (in years): "))
+
+simpleInterest = (principal * rateOfInterest * time)/100
+
+print("Simple Interest= ", simpleInterest)
