a7d8548be52609541b28cd79fa35f3e9d7507dc0
Ritupagar12 <ritupagar3@gmail.com>
2025-08-25 17:19:14 +1200

    Add String Methods Project

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+s = "hello world"
+
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(s.title())
+print(s.find("o"))
+print(s.index("w"))
+print(s.count("l"))
+print(s.isalpha())
+print(s.islower())
+print(s.isupper())
+print(s.strip())
+print(s.replace("World", "Python"))
