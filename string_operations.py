2d2024e2e5a4aedaacd4bdcdcfe181eb2e0589b0
Ritupagar12 <ritupagar3@gmail.com>
2025-08-25 16:27:42 +1200

    Add String Operations Project

---
+++ string_operations.py
@@ -0,0 +1,23 @@
+#String Operations
+s1 = "Hello"
+s2 = "World"
+
+#Concatenation
+print(s1 + " " +s2)
+
+#Repetition
+print(s1 * 3)
+
+#Indexing
+print(s1[0])
+print(s1[-1])
+
+#Slicing
+print(s1[0:3])
+print(s1[1:])
+print(s1[:4])
+print(s1[::-1])
+
+#Membership
+print("H" in s1)
+print("h" in s1)
