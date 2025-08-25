224adb7bf570c6f5e51aa8e8e290408b61daf9a3
Ritupagar12 <69349718+Ritupagar12@users.noreply.github.com>
2025-08-26 11:05:57 +1200

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,4 +1,5 @@
-number = int(input("Enter a number:"))
-
 for i in range(1, 11):
-    print(number, " X ", i, " = ", number * i)
+    for j in range(1, 11):
+        print(f"{i} X {j} = {i*j}")
+        
+    print("_" * 20)
