efc027b87a827adfde3ce6700820529b915011d9
Ritupagar12 <ritupagar3@gmail.com>
2025-08-25 21:24:34 +1200

    Add string formatting project

---
+++ string_formatting.py
@@ -0,0 +1,17 @@
+# Escape sequences
+print("Hello\nWorld")   #New Line
+print("Hello\tWorld")   #Tab
+print("He said \"Hi\"") #Double Quote inside string
+
+# Format specification symbols
+x = 10
+y = 3.456
+print(f"Integer: {x:d}, Float: {y:.2f}")
+
+# Using format() function
+name = "Ritu"
+age = 25
+print("My name is {} and I am {} years old.".format(name, age))
+print("My name is {n} and I am {a} years old.".format(n = name, a = age))
+
+
